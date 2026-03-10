7f8527fa23e0647e7a4ec43ed6d0332342db6773
stogharoww <stogharoww@gmail.com>
2026-03-10 21:58:04 +0300

    Remove build directory from repo

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CookingMew_FILE D:/Qt/Programs/CookingMew/CookingMew/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/CookingMew.exe)
-set(__QT_DEPLOY_TARGET_CookingMew_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CookingMew_RUNTIME_DLLS D:/Qt/Qt6/6.10.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/Qt6/6.10.2/mingw_64/bin/Qt6Sql.dll;D:/Qt/Qt6/6.10.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/Qt6/6.10.2/mingw_64/bin/Qt6Core.dll)
