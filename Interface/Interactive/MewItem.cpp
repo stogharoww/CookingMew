c90167c249630a64f32bef9c183c0963fe873a56
stogharoww <stogharoww@gmail.com>
2026-04-08 22:36:18 +0300

    Color scheme for MewItem updated

--- Interface/Interactive/MewItem.cpp
+++ Interface/Interactive/MewItem.cpp
@@ -13,28 +13,29 @@ MewItem::MewItem(ColorScheme &scheme, QRectF &globalRect, QGraphicsItem *parent)
     mainRect = QRectF(4, 4, globalRect.width() - 8, 500);
     setBoundingRect(mainRect);
 
-    // ====== TITLE ======
-    titleItem = new QGraphicsTextItem(this);
-    titleItem->setDefaultTextColor(_scheme.titleColor());
-    titleItem->setFont(QFont("Arial", 22));
-    titleItem->setPos(20, 20);
-    titleItem->setAcceptedMouseButtons(Qt::NoButton);
-    titleItem->setAcceptHoverEvents(false);
-
     // ====== GROUP ======
     groupItem = new QGraphicsTextItem(this);
     groupItem->setDefaultTextColor(_scheme.groupColor());
-    groupItem->setFont(QFont("Arial", 14));
-    groupItem->setPos(20, 60);
+    groupItem->setFont(QFont("Arial", 22));
+    groupItem->setPos(20, 20);
     groupItem->setAcceptedMouseButtons(Qt::NoButton);
     groupItem->setAcceptHoverEvents(false);
 
+    // ====== TITLE ======
+    titleItem = new QGraphicsTextItem(this);
+    titleItem->setDefaultTextColor(_scheme.titleColor());
+    titleItem->setFont(QFont("Arial", 14));
+    titleItem->setPos(25, 60);
+    titleItem->setAcceptedMouseButtons(Qt::NoButton);
+    titleItem->setAcceptHoverEvents(false);
+
+
     // ====== INGREDIENTS ======
     ingredientsItem = new QGraphicsTextItem(this);
-    ingredientsItem->setDefaultTextColor(_scheme.textColorGet());
+    ingredientsItem->setDefaultTextColor(_scheme.additionalColorGet());
     ingredientsItem->setFont(QFont("Arial", 12));
     ingredientsItem->setTextWidth(mainRect.width() - 40);
-    ingredientsItem->setPos(20, 90);
+    ingredientsItem->setPos(25, 90);
     ingredientsItem->setAcceptedMouseButtons(Qt::NoButton);
     ingredientsItem->setAcceptHoverEvents(false);
 
@@ -42,7 +43,7 @@ MewItem::MewItem(ColorScheme &scheme, QRectF &globalRect, QGraphicsItem *parent)
     tagItem = new QGraphicsTextItem(this);
     tagItem->setDefaultTextColor(_scheme.tagColor());
     tagItem->setFont(QFont("Arial", 12));
-    tagItem->setPos(20, 220);
+    tagItem->setPos(groupItem->boundingRect().height() + 25, 25);
     tagItem->setAcceptedMouseButtons(Qt::NoButton);
     tagItem->setAcceptHoverEvents(false);
 
@@ -51,7 +52,7 @@ MewItem::MewItem(ColorScheme &scheme, QRectF &globalRect, QGraphicsItem *parent)
     stepsItem->setDefaultTextColor(_scheme.textColorGet());
     stepsItem->setFont(QFont("Arial", 12));
     stepsItem->setTextWidth(mainRect.width() - 40);
-    stepsItem->setPos(20, 250);
+    stepsItem->setPos(25, 250);
     stepsItem->setAcceptedMouseButtons(Qt::NoButton);
     stepsItem->setAcceptHoverEvents(false);
 }
@@ -78,7 +79,7 @@ void MewItem::setContent(const QString& title,
     groupItem->setPlainText(group);
     ingredientsItem->setPlainText(ingredients);
     stepsItem->setPlainText(steps);
-    tagItem->setPlainText(tag);
+    tagItem->setPlainText("@" + tag);
 
     // ====== Обрезка ингредиентов ======
     if (ingredientsItem->boundingRect().height() > maxIngredientsHeight)
